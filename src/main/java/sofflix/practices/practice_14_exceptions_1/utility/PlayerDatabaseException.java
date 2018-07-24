1e2eca716cffdd68a10ee0db324ff4fab7f5b530
sotelofe <fsotelo87>
2018-07-23 23:21:28 -0500

    Se suben los ejercicios 11 y 12 y practicas del 8 al 14

---
+++ src/main/java/sofflix/practices/practice_14_exceptions_1/utility/PlayerDatabaseException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author Omar
+ */
+public class PlayerDatabaseException extends Exception{
+    
+    public PlayerDatabaseException(){}
+    public PlayerDatabaseException(String message){
+        super(message);
+    }
+}
