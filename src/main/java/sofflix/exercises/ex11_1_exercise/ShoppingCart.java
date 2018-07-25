041afda96b3da32998c9668bd52beae25a9e9925
sotelofe <fsotelo87>
2018-07-25 00:23:08 -0500

    Se suben los ejercicios y practicas restantes

---
+++ src/main/java/sofflix/exercises/ex11_1_exercise/ShoppingCart.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex11_1_exercise;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Omar
+ */
+public class ShoppingCart {
+    public static void main(String[] args){
+	// Declare a LocalDateTime object, orderDate
+        LocalDateTime orderDate; 
+        
+	// Initialize the orderDate to the current date and time. Print it.
+        orderDate = LocalDateTime.now();
+        System.out.println(orderDate);
+	// Format orderDate using ISO_LOCAL_DATE; Print it.
+        String orderDateFormat = orderDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(orderDateFormat);
+    }
+}
---
+++ src/main/java/sofflix/exercises/ex11_2_exercise/ShoppingCart.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex11_2_exercise;
+
+/**
+ *
+ * @author Omar
+ */
+public class ShoppingCart {
+     public static void main(String[] args) {
+        String name;
+        int age;
+        
+       
+        if(args.length <2){
+             // Print an error message if fewer than 2 args are passed in.
+            System.out.println("Se requieren dos argumentos de entrada...");
+        }else{
+            // Parse the args array to populate name and age.  
+            name = args[0];
+            age = Integer.parseInt(args[1]);
+            System.out.println("name: "+name);
+            System.out.println("age: "+age);
+        }
+        
+	
+        
+        
+    }
+}
